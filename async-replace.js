d17829eca67f7b79fcfaba3ab33c859d99c19520
Samuel Ronce <sam@webcreative5.net>
2020-01-09 16:20:17 +0100

    use const/let

--- async-replace.js
+++ async-replace.js
@@ -1,17 +1,17 @@
 'use strict'
 
 function toLocal(regexp) {
-    var flags = '';
+    let flags = '';
     if (regexp.ignoreCase) flags += 'i';
-    var copy = new RegExp(regexp.source, flags);
+    const copy = new RegExp(regexp.source, flags);
     return copy;
 }
 
 function replaceLocal(string, regexp, replacer) {
     return new Promise((resolve, reject) => {
-        var matched = string.match(regexp);
+        const matched = string.match(regexp);
         if (!matched) return resolve(string)
-        var args = matched.slice();
+        const args = matched.slice();
         args.push(matched.index);
         args.push(matched.input);
         args.push(function (err, newString) {
@@ -22,24 +22,24 @@ function replaceLocal(string, regexp, replacer) {
     })
 }
 
-module.exports = function (string, regexp, replacer = function() {}) {
+module.exports = function (string, regexp, replacer = function () {}) {
     if (!regexp.global) return replaceLocal(string, regexp, replacer);
 
-    var matched = string.match(regexp);
+    const matched = string.match(regexp);
 
     if (!matched) {
         return Promise.resolve(string)
     }
 
-    // matched is an array of matched strings
-    var result = [];
-    var i = 0;
-    var index = 0;
-    var copy = toLocal(regexp);
-    var callbacks = [];
+    let i = 0;
+    let index = 0;
+
+    const result = [];
+    const copy = toLocal(regexp);
+    const callbacks = [];
     while (matched.length > 0) {
-        var subString = matched.shift();
-        var nextIndex = string.indexOf(subString, index);
+        const subString = matched.shift();
+        const nextIndex = string.indexOf(subString, index);
         result[i] = string.slice(index, nextIndex);
         i++;
         (function (j, index, subString) {
